8c94b3744198dde5372796b30f206e6e148c1594
Valery Bashkatov <valery@bashkatov.org>
2016-12-25 23:28:31 +0300

    2.0.2: Fixed error descriptions logic.

--- Sources/KeychainError.swift
+++ Sources/KeychainError.swift
@@ -15,24 +15,25 @@ public struct KeychainError: Error, CustomStringConvertible {
     
     // MARK: - Properties
     
+    /// The error codes descriptions.
+    private static let descriptions = [
+        -4: "The function or operation is not implemented",
+        -50: "One or more parameters passed to a function were not valid",
+        -108: "Failed to allocate memory",
+        -25291: "No keychain is available",
+        -25299: "An item with the same primary key attributes already exists",
+        -25300: "The item cannot be found",
+        -25308: "Interaction with the Security Server is not allowed",
+        -26275: "Unable to decode the provided data",
+        -34018: "Internal error when a required entitlement isn't present. Keychain entitlement required"
+    ]
+    
     /// The error code.
     public let code: Int
     
     /// The description.
     public var description: String {
-        let descriptions = [
-            -4: "The function or operation is not implemented",
-            -50: "One or more parameters passed to a function were not valid",
-            -108: "Failed to allocate memory",
-            -25291: "No keychain is available",
-            -25299: "An item with the same primary key attributes already exists",
-            -25300: "The item cannot be found",
-            -25308: "Interaction with the Security Server is not allowed",
-            -26275: "Unable to decode the provided data",
-            -34018: "Internal error when a required entitlement isn't present. Keychain entitlement required"
-        ]
-        
-        return descriptions[code] ?? "Unknown error (\(code))"
+        return KeychainError.descriptions[code] ?? "Unknown error (\(code))"
     }
     
     // MARK: - Initialization
